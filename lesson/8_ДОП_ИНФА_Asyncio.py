80c9587c316aebd21ea3671d97a039e6f5f5e3fb
wave-silent <danillogutov.74@mail.ru>
2025-07-28 13:11:28 +0500

    Update 8_ДОП_ИНФА_Asyncio.py

--- lesson/8_ДОП_ИНФА_Asyncio.py
+++ lesson/8_ДОП_ИНФА_Asyncio.py
@@ -1,18 +1,23 @@
+'''
+Asyncio - это специальная библиотека, используемая для реализации концепции асинхронного и конкурентного программирования
+
+'''
+
 import asyncio
 
-async def send_time(second: int) -> None:
-    #n = 0
-    await asyncio.sleep(second)
-    #n += second
-    print(f'Прошло {second} секунд')
+async def send_hello() -> None:
+    await asyncio.sleep(2)
+    print('Hello')
 
-async def main() -> None:
-    task_1 = asyncio.create_task(send_time(2))    # различные обьекты корутин! 
-    task_2 = asyncio.create_task(send_time(5))
-    
+async def send_bye() -> None:
+    await asyncio.sleep(1)
+    print('Bye')
+
+async def main():
+    task_1 = asyncio.create_task(send_hello())
+    task_2 = asyncio.create_task(send_bye())
+
     await task_1
     await task_2
-    
 
-if __name__ == '__main__':
-    asyncio.run(main())
+asyncio.run(main())
